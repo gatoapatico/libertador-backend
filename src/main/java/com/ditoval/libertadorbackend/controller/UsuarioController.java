8b2c34b201d88974efeead1b47e16343a689a16d
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-12-21 17:08:34 -0500

    Crud + REST API -> usuarios

---
+++ src/main/java/com/ditoval/libertadorbackend/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.ditoval.libertadorbackend.controller;
+
+import com.ditoval.libertadorbackend.entity.Usuario;
+import com.ditoval.libertadorbackend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @PostMapping
+    public Usuario addUsuario(@RequestBody Usuario usuario) {
+        return service.addUsuario(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getUsuario(@PathVariable int id) {
+        return service.getUsuario(id);
+    }
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return service.getAllUsuarios();
+    }
+
+    @PutMapping("/{id}")
+    public Usuario updateUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+        return service.updateUsuario(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable int id) {
+        service.deleteUsuario(id);
+    }
+}
---
+++ src/main/java/com/ditoval/libertadorbackend/entity/Usuario.java
@@ -0,0 +1,102 @@
+package com.ditoval.libertadorbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String dni;
+    private String telefono;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email, String password, String dni, String telefono) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.dni = dni;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dni='" + dni + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+}
