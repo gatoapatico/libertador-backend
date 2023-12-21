6fd768f69f6c7afc2b4c5fa77e96d75d8f165631
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-12-21 16:24:57 -0500

    Libertador Backend Start

---
+++ src/main/java/com/ditoval/libertadorbackend/LibertadorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ditoval.libertadorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibertadorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibertadorBackendApplication.class, args);
+    }
+
+}
