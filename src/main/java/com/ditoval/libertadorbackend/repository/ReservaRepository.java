6136026a64992fe3b66347518e4e26951523eb09
ALVARO FERNANDEZ <aferzu@gmail.com>
2024-01-04 14:25:28 -0500

    Delete src directory

--- src/main/java/com/ditoval/libertadorbackend/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ditoval.libertadorbackend.repository;
-
-import com.ditoval.libertadorbackend.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
-}
